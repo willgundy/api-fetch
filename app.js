b616cd22881d7554ae56c61f82ea8ff0301d211d
Will Gundy <willgunderson503@gmail.com>
2022-03-03 15:34:17 -0800

    Added event listener to selector

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const template = document.querySelector('#template');
 const selector = document.querySelector('#api-select');
 const list = document.querySelector('#list');
 
+
 //console.log(template, selector, list);
 
 async function loadPokedex() {
-    const pokedex = await getPokedex;
+    const pokedex = await getPokedex();
 
     for (let pokemon of pokedex) {
-        console.log(pokemon);
 
-        const clone = template.textContent.cloneNode(true);
+        const clone = template.content.cloneNode(true);
         const name = clone.querySelector('h2');
         const type = clone.querySelector('h6');
         const image = clone.querySelector('img');
 
         name.textContent = 'Name: ' + pokemon.pokemon;
-        type.textContent = 'Egg: ' + pokemon.egg_group1;
+        type.textContent = 'Egg: ' + pokemon.egg_group_1;
         image.src = pokemon.url_image;
         image.alt = pokemon.pokedex;
 
@@ -31,12 +31,11 @@ async function loadPokedex() {
 }
 
 async function useTheForce() {
-    const peopleInStarWars = await theForce;
+    const peopleInStarWars = await theForce();
 
     for (let character of peopleInStarWars) {
-        console.log(character);
 
-        const clone = template.textContent.cloneNode(true);
+        const clone = template.content.cloneNode(true);
         const name = clone.querySelector('h2');
         const type = clone.querySelector('h6');
 
@@ -49,6 +48,20 @@ async function useTheForce() {
 }
 
 // set event listeners 
+selector.addEventListener('change', (event) => {
+    const selectedAPI = event.target.value;
+
+    if (selectedAPI === 'pokemon') {
+        list.innerHTML = '';
+        loadPokedex();
+    } else if (selectedAPI === 'star-wars') {
+        list.innerHTML = '';
+        useTheForce();
+    } else {
+        list.innerHTML = '';
+        list.innerHTML = 'Select an API!';
+    }
+});
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
